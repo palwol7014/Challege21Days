1d0c32ec5669a630a8fcc23d3ab202724347c6ef
palwol7014 <wolnarek.pawel@gmail.com>
2023-02-09 18:23:04 +0100

    Zadanie domowe dzień 4

--- Challege21Days/Challege21Days/Program.cs
+++ Challege21Days/Challege21Days/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "Ewa";
+char gender = 'k';
+short age = 33;
+
+if (gender == 'k' && age < 30)
+	Console.WriteLine("Kobieta poniżej 30 lat.");
+
+if (name == "Ewa" && age == 33)
+	Console.WriteLine("Ewa lat {0}", age);
+
+if (gender == 'm' && age < 18)
+	Console.WriteLine("Niepełnoletni mężczyzna.");
+
+if (gender == 'k' && age >= 60)
+	Console.WriteLine("Kobieta w wieku emerytalnym.");
+
+if (gender == 'm' && age >= 65)
+	Console.WriteLine("Meżczyzna w wieku emerytalnym.");
