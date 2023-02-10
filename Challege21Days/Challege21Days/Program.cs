e2d7208be6acc9c1ba5703e3855695b0d71d0dcf
palwol7014 <wolnarek.pawel@gmail.com>
2023-02-10 11:07:37 +0100

    Zadanie z dnia 5

--- Challege21Days/Challege21Days/Program.cs
+++ Challege21Days/Challege21Days/Program.cs
@@ -1,13 +1,29 @@
-﻿int[] digits = new int[10];
-int num = 542641100;
+﻿using Challege21Days;
 
-Console.WriteLine("Dla liczby {0}\n", num);
+List<Employee> employees = new List<Employee>();
 
-do
+int[] grades = new int[5];
+Random random = new Random();
+
+employees.Add(new Employee("Adam", "####", 18, new int[] { 5, 9, 4, 3,1 } ));
+employees.Add(new Employee("Zuzia", "####", 36, new int[] { 6, 4, 5, 3, 4 }));
+employees.Add(new Employee("Robert", "####", 45, new int[] { 1, 2, 3, 4, 5 }));
+
+//There may several employee which gained the biggest score.
+List<Employee> maxScoreEmployess = new List<Employee>();
+
+foreach(var emp in employees)
+	if(maxScoreEmployess.Count == 0) maxScoreEmployess.Add(emp);
+	else if (maxScoreEmployess[0].Scores == emp.Scores) maxScoreEmployess.Add(emp);
+	else if (maxScoreEmployess[0].Scores < emp.Scores)
+	{
+		maxScoreEmployess.Clear();
+		maxScoreEmployess.Add(emp);
+	}
+
+Console.WriteLine("Pracownicy z nawyższą liczbą pkt:");
+foreach (var emp in maxScoreEmployess)
 {
-	digits[num % 10]++;
-	num /= 10;
-}while(num > 0);
-
-for(int i = 0; i < digits.Length; i++)
-	Console.WriteLine("{0} => {1}", i , digits[i]);
+	emp.show();
+	Console.WriteLine("");
+}
