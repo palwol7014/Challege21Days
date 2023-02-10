68be886572b023d597c23ccbfb5b55bcfe68725e
palwol7014 <wolnarek.pawel@gmail.com>
2023-02-10 11:05:05 +0100

    Zadanie domowe z dnia 6

---
+++ Challege21Days/Challege21Days/Employee.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challege21Days
+{
+	internal class Employee
+	{
+		private string name;
+		private string surname;
+		private short age;
+		private int scores;
+
+		public int Scores
+		{
+			get
+			{
+				return scores;
+			}
+		}
+
+		public void show()
+		{
+
+			Console.WriteLine("Imię: {0}", name);
+			Console.WriteLine("Nazwisko: {0}", surname);
+			Console.WriteLine("Wiek: {0}", age);
+			Console.WriteLine("punkty: {0}", scores);
+		}
+		public Employee(string name, string surname, short age, int[] tabScores)
+		{
+			this.name = name;
+			this.surname = surname;
+			this.age = age;
+
+			foreach (var item in tabScores)
+				scores += item;
+		}
+	}
+}
